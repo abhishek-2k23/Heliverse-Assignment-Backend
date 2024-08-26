a75b8201be1803e6265670ca0edd1faaf26bdecb
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-26 20:51:51 +0530

    server up with db connection

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import dbConnection from "./Configuration/dbConnect.js"
+import dotenv from "dotenv"
+import cors from "cors"
+import router from "./Routers/router.js"
+
+//initialize
+const app = express()
+dotenv.config()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+dbConnection()
+
+app.listen(process.env.DB_PORT, () => {
+  console.log("server is up at ", process.env.DB_PORT)
+})
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome to the Home Route of the Blog App Backend`)
+})
+
+//route mounting
+app.use("/api", router)
