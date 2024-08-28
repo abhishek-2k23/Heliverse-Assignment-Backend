e25517ba12a875b407e4dcdf315e21049191edd7
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-28 14:03:40 +0530

    team routes

--- Routers/router.js
+++ Routers/router.js
@@ -8,12 +8,21 @@ import { addUser } from '../Controllers/AddUser.controller.js';
 import { updateUser } from '../Controllers/UpdateUser.controller.js';
 import { deleteUser } from '../Controllers/DeleteUser.controller.js';
 import { getUsers, searchUser } from '../Controllers/GetUser.controller.js';
+import { createTeam } from '../Controllers/CreateTeam.controller.js';
+import { addUsersToTeam } from '../Controllers/AddUser.team.controller.js';
+import { getTeam } from '../Controllers/GetTeam.controller.js';
+import { deleteTeam } from '../Controllers/DeleteTeam.controller.js';
 
 router.post("/users",addUser);
 router.put("/users/:id",updateUser);
 router.delete("/users/:id", deleteUser);
 router.get("/users",getUsers);
 router.get("/users/search",searchUser);
+
+router.post("/team/create",createTeam);
+router.post("/team/addUser/:teamId",addUsersToTeam);
+router.get("/team/getTeam", getTeam);
+router.delete("/team/deleteTeam/:id", deleteTeam);
 
 
 //export
